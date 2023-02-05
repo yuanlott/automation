61b3667a6c3fb1b5edf373f866fd8cb8f1aa369e
Lei Yuan <yuanlottawa85@gmail.com>
2023-02-05 13:35:34 -0500

    Break setup into 2 subsections

--- examples/checkserver/checkserver_script.py
+++ examples/checkserver/checkserver_script.py
@@ -12,6 +12,7 @@ import logging
 # Needed for aetest script
 from pyats import aetest
 from genie.libs.parser.utils.entry_points import add_parser
+
 # my lib
 from extraparsers.top import Top
 
@@ -27,26 +28,35 @@ log = logging.getLogger()
 # You can have one of no CommonSetup
 # CommonSetup can be named whatever you want
 
+
 class common_setup(aetest.CommonSetup):
-    """ Common Setup section """
+    """Common Setup section"""
 
     # CommonSetup have subsection.
     # You can have 1 to as many subsection as wanted
 
-    # First subsection
+    # 1st subsection
     @aetest.subsection
     def connect(self, testscript, testbed):
-        """ Common Setup subsection """
-        log.info("Aetest Common Setup ")
+        """Common Setup subsection"""
+        log.info("Aetest Common Setup: connect to device")
         device = testbed.devices["egypt"]
         # Connecting to the devices using the default connection
         device.connect()
 
         # Save it in testscript parmaeters to be able to use it from other
         # test sections
-        testscript.parameters['uut'] = device
+        testscript.parameters["uut"] = device
+
+    # 2nd subsection
+    @aetest.subsection
+    def register_parser(self, section):
+        log.info("Aetest Common Setup: add parser")
         # register my parser
         add_parser(Top, "linux")
+
+        # Save it in testscript parmaeters to be able to use it from other
+
 
 ###################################################################
 ###                     TESTCASES SECTION                       ###
@@ -55,9 +65,10 @@ class common_setup(aetest.CommonSetup):
 # This is how to create a testcase
 # You can have 0 to as many testcase as wanted
 
+
 # Testcase name : tc_one
 class test_top(aetest.Testcase):
-    """ This is user Testcases section """
+    """This is user Testcases section"""
 
     # Testcases are divided into 3 sections
     # Setup, Test and Cleanup.
@@ -66,20 +77,22 @@ class test_top(aetest.Testcase):
     @aetest.setup
     def send_command(self, uut):
         # Get device output
-        self.output = uut.parse('top -n 1 -b')
+        self.output = uut.parse("top -n 1 -b")
 
         # Configuration can also be send
         # uut.configure('some configuration')
+
 
 #####################################################################
 ####                       COMMON CLEANUP SECTION                 ###
 #####################################################################
 
+
 # This is how to create a CommonCleanup
 # You can have 0 , or 1 CommonCleanup.
 # CommonCleanup can be named whatever you want :)
 class common_cleanup(aetest.CommonCleanup):
-    """ Common Cleanup for Sample Test """
+    """Common Cleanup for Sample Test"""
 
     # CommonCleanup follow exactly the same rule as CommonSetup regarding
     # subsection
@@ -87,5 +100,5 @@ class common_cleanup(aetest.CommonCleanup):
 
     @aetest.subsection
     def disconnect(self, uut):
-        """ Common Cleanup Subsection """
+        """Common Cleanup Subsection"""
         uut.disconnect()
