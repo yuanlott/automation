7c38aaeeb01fb702c7e7f063f936bdc4cf9da77c
Lei Yuan <yuanlottawa85@gmail.com>
2023-02-04 20:49:49 -0500

    Removed deleted dir and files

--- parserlib/__init__.py
+++
@@ -1,6 +0,0 @@
-# Paackages where parser modules exist must have following lines in __init__.py
-# otherwise the parser through runtime exception during use
-from genie import abstract
-
-abstract.declare_package(__name__)
-abstract.declare_token(__name__)
